83b1c79dd3a95459325ccb769b81f759f5e1c028
bianyunjian <byj626680108@gmail.com>
2021-12-21 14:40:50 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,15 +1,15 @@
 window.API = "https://web3go.xyz/portal/";
 window.POLKADOT_API = "https://web3go.xyz/polkadot/";
 window.KUSAMA_API = "https://web3go.xyz/kusama/";
-//window.MOONRIVER_API = "https://web3go.xyz/moonriver/";
+window.MOONRIVER_API = "https://web3go.xyz/moonriver/";
 window.RMRK_API = "https://web3go.xyz/rmrk/";
 
 
 //window.API = "http://localhost:10000/";
 //window.POLKADOT_API = "http://localhost:20002/";
 //window.KUSAMA_API = "http://localhost:20003/";
-window.MOONRIVER_API = "http://localhost:20004/";
-// window.RMRK_API = "http://localhost:20005/";
+//window.MOONRIVER_API = "http://localhost:20004/";
+//window.RMRK_API = "http://localhost:20005/";
 
 
 console.log("window.API：", window.API);
