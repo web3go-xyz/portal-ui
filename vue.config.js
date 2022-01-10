04202cf08f22c72a8bd9b02b0995b2b72f237e20
zhangzhi <474294484@qq.com>
2022-01-10 14:26:03 +0800

    vue-cli

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    lintOnSave: false,
+    configureWebpack: (config) => {
+        config.module.rules.push({
+            test: /\.js$/,
+            use: [{
+                loader: require.resolve('@open-wc/webpack-import-meta-loader'),
+            }]
+        })
+    },
+    chainWebpack: config => {
+        // 用cdn方式引入
+        config.externals({
+            'vue': 'Vue',
+            'element-ui': 'ELEMENT',
+            'echarts': 'echarts',
+            'axios': 'axios',
+        })
+    }
+}
