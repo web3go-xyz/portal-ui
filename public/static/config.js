a97f25a6d56ed1f413d17f103e6d4a2d3315d819
bianyunjian <byj626680108@gmail.com>
2022-01-12 17:28:46 +0800

    add MOONBEAM_API

--- public/static/config.js
+++ public/static/config.js
@@ -5,16 +5,19 @@ window.KUSAMA_API = "https://web3go.xyz/kusama/";
 window.MOONRIVER_API = "https://web3go.xyz/moonriver/";
 window.RMRK_API = "https://web3go.xyz/rmrk/";
 
+window.MOONBEAM_API = "https://web3go.xyz/moonbeam/";
 
 //window.API = "http://localhost:10000/";
 //window.POLKADOT_API = "http://localhost:20002/";
 //window.KUSAMA_API = "http://localhost:20003/";
 //window.MOONRIVER_API = "http://localhost:20004/";
 //window.RMRK_API = "http://localhost:20005/";
-
+//window.MOONBEAM_API = "http://localhost:20014/";
 
 console.log("window.API：", window.API);
 console.log("window.POLKADOT_API", window.POLKADOT_API);
 console.log("window.KUSAMA_API", window.KUSAMA_API);
 console.log("window.MOONRIVER_API", window.MOONRIVER_API);
 console.log("window.RMRK_API", window.RMRK_API);
+
+console.log("window.MOONBEAM_API", window.MOONBEAM_API);
