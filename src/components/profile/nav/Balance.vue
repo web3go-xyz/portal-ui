a4b98b1fbb8b54b77c10b02f5eb3d473f4046581
zhangzhi <474294484@qq.com>
2022-01-14 15:43:43 +0800

    profile balance Detail new ui

--- src/components/profile/nav/Balance.vue
+++ src/components/profile/nav/Balance.vue
@@ -24,8 +24,6 @@
             )
           "
           style="width: 100%"
-          :row-class-name="getRowClass"
-          @row-click="rowClick"
         >
           <el-table-column label="Token" width="225">
             <template slot-scope="scope">
@@ -81,10 +79,45 @@
               <div v-else>$ {{ scope.row.totalPrice | format2 }}</div>
             </template>
           </el-table-column>
+          <el-table-column width="80" label=" " align="center">
+            <template slot-scope="scope">
+              <i
+                v-if="
+                  currentRow &&
+                  scope.row.value == currentRow.value &&
+                  detailLoading
+                "
+                class="el-icon-loading"
+              ></i>
+              <span v-else class="view hover-item" @click="rowClick(scope.row)"
+                >view</span
+              >
+            </template>
+          </el-table-column>
         </el-table>
       </div>
-      <div v-if="currentRow" v-loading="detailLoading" class="right-table">
+    </div>
+    <div class="pagination-wrap">
+      <div class="left">
+        <el-pagination
+          layout="prev, pager, next"
+          :total="filterBalanceNavData.length"
+          :current-page.sync="tableParams1.pageIndex"
+          :page-size="tableParams1.pageSize"
+          @current-change="tableCurrentChange1"
+        >
+        </el-pagination>
+      </div>
+    </div>
+    <el-dialog
+      center
+      :title="currentRow && shorterAddress(currentRow.value)"
+      :visible.sync="visbile"
+      width="600"
+    >
+      <div class="right-table">
         <el-table
+          v-loading="detailLoading"
           :data="
             detailData.slice(
               (tableParams2.pageIndex - 1) * tableParams2.pageSize,
@@ -127,16 +160,14 @@
           </el-table-column>
           <el-table-column width="30" class-name="icon-cell">
             <template>
-              <div>
-                <img
-                  class="trans"
-                  src="@/assets/images/profile/trans.png"
-                  alt=""
-                />
-              </div>
+              <img
+                class="trans"
+                src="@/assets/images/profile/trans.png"
+                alt=""
+              />
             </template>
           </el-table-column>
-          <el-table-column width="145">
+          <el-table-column>
             <template slot-scope="scope">
               <div class="detail-to">
                 <div class="item-title">
@@ -174,27 +205,9 @@
               </div>
             </template>
           </el-table-column>
-          <div class="empty" slot="empty">
-            <div class="img-wrap">
-              <img src="@/assets/images/profile/none.png" alt="" />
-            </div>
-            <div class="text">No data…</div>
-          </div>
         </el-table>
       </div>
-    </div>
-    <div class="pagination-wrap">
-      <div class="left">
-        <el-pagination
-          layout="prev, pager, next"
-          :total="filterBalanceNavData.length"
-          :current-page.sync="tableParams1.pageIndex"
-          :page-size="tableParams1.pageSize"
-          @current-change="tableCurrentChange1"
-        >
-        </el-pagination>
-      </div>
-      <div class="right">
+      <div class="pagination-wrap">
         <el-pagination
           v-if="currentRow"
           layout="prev, pager, next"
@@ -204,7 +217,7 @@
         >
         </el-pagination>
       </div>
-    </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -223,6 +236,7 @@ export default {
   },
   data() {
     return {
+      visbile: false,
       detailLoading: false,
       currentRow: null,
       detailData: [],
@@ -353,6 +367,11 @@ export default {
         });
         console.log("detailData", detailData);
         this.detailData = detailData;
+        if(this.detailData.length){
+          this.visbile = true;
+        }else{
+          this.$message.warning('The wallet has no data');
+        }
       });
     },
     clearTable2() {
@@ -376,6 +395,42 @@ export default {
 
 <style lang="less" scoped>
 .profile-balance-page {
+  .right-table {
+    background: #f5f7f9;
+    flex: 4;
+    overflow: hidden;
+    /deep/ .el-table {
+      .el-table__body {
+        border-spacing: 0 !important;
+        .el-table__cell {
+          padding-top: 5px;
+          padding-bottom: 5px;
+        }
+      }
+      .el-table__body tr:hover {
+        box-shadow: none !important;
+      }
+      td.el-table__cell,
+      th.el-table__cell.is-leaf {
+        border-bottom: 1px solid rgba(233, 233, 233, 1) !important;
+        border-radius: 0 !important;
+      }
+      .cell {
+        padding-left: 10px;
+        color: rgba(41, 40, 40, 0.6);
+      }
+      th {
+        height: 48px;
+      }
+      .has-gutter {
+        display: none;
+      }
+    }
+  }
+  .view {
+    font-size: 14px;
+    color: #38cb98;
+  }
   .tag-wrap {
     .tag {
       display: inline-block;
@@ -405,131 +460,70 @@ export default {
       flex: 5;
       overflow: hidden;
       /deep/ .el-table {
-        .el-table__row {
-          cursor: pointer;
-        }
-        .el-table__row.active {
-          background: rgba(245, 247, 249, 1);
-        }
         .el-table__body {
           border-spacing: 0 !important;
           .el-table__cell {
             padding-top: 5px;
             padding-bottom: 5px;
           }
-        }
-        .el-table__body tr:hover {
-          box-shadow: none !important;
-          background-color: #f5f7fa;
-        }
-        td.el-table__cell,
-        th.el-table__cell.is-leaf {
-          border-bottom: 1px solid rgba(233, 233, 233, 1) !important;
-          border-radius: 0 !important;
-        }
-        .cell {
-          padding-left: 10px;
-          color: rgba(41, 40, 40, 0.6);
-        }
-      }
-    }
-    .right-table {
-      background: #f5f7f9;
-      flex: 4;
-      overflow: hidden;
-      /deep/ .el-table {
-        .el-table__body {
-          border-spacing: 0 !important;
-          .el-table__cell {
-            padding-top: 5px;
-            padding-bottom: 5px;
-          }
-        }
-        .el-table__body tr:hover {
-          box-shadow: none !important;
-          background-color: #f5f7fa;
-        }
-        td.el-table__cell,
-        th.el-table__cell.is-leaf {
-          border-bottom: 1px solid rgba(233, 233, 233, 1) !important;
-          border-radius: 0 !important;
-        }
-        .cell {
-          padding-left: 10px;
-          color: rgba(41, 40, 40, 0.6);
-        }
-        th {
-          height: 48px;
         }
 
-        .el-table__empty-block {
-          border-radius: 0;
-          background: #f5f7f9 !important;
+        td.el-table__cell,
+        th.el-table__cell.is-leaf {
+          border-bottom: 1px solid rgba(233, 233, 233, 1) !important;
+          border-radius: 0 !important;
         }
-        tr {
-          background: #f5f7f9;
+        .cell {
+          padding-left: 10px;
+          color: rgba(41, 40, 40, 0.6);
         }
       }
     }
-    .token-cell {
-      display: flex;
-      align-items: center;
-      .left {
-        width: 26px;
-        height: auto;
+  }
+  .token-cell {
+    display: flex;
+    align-items: center;
+    .left {
+      width: 26px;
+      height: auto;
+    }
+    .right {
+      flex: 1;
+      margin: 0 11px;
+      .item-title {
+        font-size: 14px;
+        font-weight: bold;
+        color: #545353;
       }
-      .right {
-        flex: 1;
-        margin: 0 11px;
-        .item-title {
-          font-size: 14px;
-          font-weight: bold;
-          color: #545353;
-        }
-        .item-text {
-          display: flex;
-          align-items: center;
-          font-family: "Rubik";
-          font-size: 12px;
-          color: #a9a9a9;
-          .copy {
-            margin-left: 8px;
-            width: 16px;
-            height: 16px;
-            opacity: 0.6;
-            cursor: pointer;
-            &:hover {
-              opacity: 0.4;
-            }
+      .item-text {
+        display: flex;
+        align-items: center;
+        font-family: "Rubik";
+        font-size: 12px;
+        color: #a9a9a9;
+        .copy {
+          margin-left: 8px;
+          width: 16px;
+          height: 16px;
+          opacity: 0.6;
+          cursor: pointer;
+          &:hover {
+            opacity: 0.4;
           }
         }
       }
     }
-    .detail-from {
-      display: flex;
-      align-items: center;
-      .left {
-        width: 26px;
-        height: auto;
-      }
-      .right {
-        flex: 1;
-        margin: 0 11px;
-        .item-title {
-          font-size: 14px;
-          color: #7f7e7e;
-        }
-        .item-text {
-          font-size: 14px;
-          color: #545353;
-        }
-      }
+  }
+  .detail-from {
+    display: flex;
+    align-items: center;
+    .left {
+      width: 26px;
+      height: auto;
     }
-    .trans {
-      width: 16px;
-      height: 16px;
-    }
-    .detail-to {
+    .right {
+      flex: 1;
+      margin: 0 11px;
       .item-title {
         font-size: 14px;
         color: #7f7e7e;
@@ -539,28 +533,44 @@ export default {
         color: #545353;
       }
     }
-    .num-change-wrap {
-      display: flex;
-      align-items: center;
-      .left {
-        width: 26px;
-        height: auto;
-        margin-right: 8px;
-      }
-      .blue {
-        font-size: 14px;
-        color: #38cb98;
-      }
-      .red {
-        font-size: 14px;
-        color: #f53f3f;
-      }
+  }
+
+  .trans {
+    width: 16px;
+    height: 16px;
+    display: block;
+  }
+
+  .detail-to {
+    .item-title {
+      font-size: 14px;
+      color: #7f7e7e;
+    }
+    .item-text {
+      font-size: 14px;
+      color: #545353;
+    }
+  }
+  .num-change-wrap {
+    display: flex;
+    align-items: center;
+    .left {
+      width: 26px;
+      height: auto;
+      margin-right: 8px;
+    }
+    .blue {
+      font-size: 14px;
+      color: #38cb98;
+    }
+    .red {
+      font-size: 14px;
+      color: #f53f3f;
     }
   }
   .pagination-wrap {
     text-align: center;
     padding-top: 20px;
-    display: flex;
     .left {
       flex: 1;
     }
@@ -588,5 +598,16 @@ export default {
       color: rgba(41, 40, 40, 0.6);
     }
   }
+  // /deep/ .detail-dialog {
+  //   border-radius: 10px;
+  //   .el-dialog__header{
+  //     padding-top:24px;
+  //   }
+  //   .el-dialog__title {
+  //     font-size: 24px;
+  //     font-weight: bold;
+  //     color: #262626;
+  //   }
+  // }
 }
 </style>
