f8ac205d5df625499c8e20a388d61ad8884366c8
MicJordan <willzhaoshihao@gmail.com>
2022-04-13 07:41:09 +0800

    Add inline documentation for api services using

--- public/static/config.js
+++ public/static/config.js
@@ -1,13 +1,13 @@
-window.BASE_API = "https://web3go.xyz";
-window.API = "https://web3go.xyz/portal/";
-window.POLKADOT_API = "https://web3go.xyz/polkadot/";
-window.KUSAMA_API = "https://web3go.xyz/kusama/";
-window.MOONRIVER_API = "https://web3go.xyz/moonriver/";
-window.RMRK_API = "https://web3go.xyz/rmrk/";
+window.BASE_API = "https://web3go.xyz"; //Prefix for all target api service
+window.API = "https://web3go.xyz/portal/"; //Prefix for api in portal service
+window.POLKADOT_API = "https://web3go.xyz/polkadot/"; //Prefix for all polkadot api
+window.KUSAMA_API = "https://web3go.xyz/kusama/"; //Prefix for all kusama api
+window.MOONRIVER_API = "https://web3go.xyz/moonriver/"; //Prefix for all moonriver api
+window.RMRK_API = "https://web3go.xyz/rmrk/"; //Prefix for all rmrk api
 
-window.MOONBEAM_API = "https://web3go.xyz/moonbeam/";
+window.MOONBEAM_API = "https://web3go.xyz/moonbeam/"; //Prefix for all moonbeam api
 
-window.BIFROST_STAKING_API = "https://web3go.xyz/bifrost-staking/";
+window.BIFROST_STAKING_API = "https://web3go.xyz/bifrost-staking/"; //Prefix for all bifrost staking api
 
 //window.API = "http://localhost:10000/";
 //window.POLKADOT_API = "http://localhost:20002/";
